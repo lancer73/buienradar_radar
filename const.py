f1dbab4af0b52bf1e732eb5a1a1bfa00b248138b
lancer73 <33584546+lancer73@users.noreply.github.com>
2025-06-12 18:29:11 +0200

    Create const.py

---
+++ const.py
@@ -0,0 +1,13 @@
+"""Constants for the Buienradar Regenradar integration."""
+
+# Integration domain
+DOMAIN = "buienradar_radar"
+
+# Default values
+DEFAULT_NAME = "BuienRadar Regenradar"
+
+# Configuration keys
+CONF_IMAGE_REFRESH_SECONDS = "image_refresh_seconds"
+
+# API URL for the radar image
+RADAR_URL = "https://api.buienradar.nl/image/1.0/RadarMapNL?w=500&h=512"
